e9b15dddd03af503751f5216fc56d83b780e451f
anjaliyadav <anjali819168@gmail.com>
2026-04-07 22:00:29 +0530

    final backend fix for render

--- backend/server.js
+++ backend/server.js
@@ -8,24 +8,21 @@ dotenv.config();
 const app = express();
 
 /* CORS */
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 app.use(express.json());
 
-/* Routes */
-app.use("/api", authRoutes);
-
-/* Home test route */
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
-/* MongoDB connection */
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB error:", err));
 
-/* Start server */
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Backend running on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
 });
+
